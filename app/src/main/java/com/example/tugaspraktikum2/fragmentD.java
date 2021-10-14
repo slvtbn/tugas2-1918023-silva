11e78aab75490d3bd27ca590c6a827a0f540c8cf
SILVA TAMBUNAN <silvaanjelina07@gmail.com>
2021-10-14 17:22:55 +0900

    first commit

---
+++ app/src/main/java/com/example/tugaspraktikum2/fragmentD.java
@@ -0,0 +1,24 @@
+package com.example.tugaspraktikum2;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class fragmentD extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_d, container, false);
+    }
+}
